8e5f9c5da951fd864258a939e5b3da68c7e1e51c
seeker105 <karavol27@gmail.com>
2017-09-28 12:12:39 -0500

    All code appears to be working

--- 3a-controllers-and-components/js/additionService.js
+++ 3a-controllers-and-components/js/additionService.js
@@ -10,52 +10,23 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
     this.rightCost = INITIAL_RIGHT_COST
     this.autoclickerCount = 0
     let intervals = []
-    this.rightActive = false
-    this.rightColor = 'lightgray'
-    this.leftActive = false
-    this.leftColor = 'lightgray'
-    this.resetActive = false
-    this.resetColor = 'lightgray'
-
-    // updatePage = () => {
-    //     if (this.total > 0 || this.addend > 1 || this.autoclickerCount > 0) {
-    //         this.resetActive = true
-    //         this.resetColor = 'white'
-    //     } else {
-    //         this.resetActive = false
-    //         this.resetColor = 'lightgray'
-    //     }
-
-    //     if (this.total >= this.leftCost) {
-    //         this.leftActive = true
-    //         this.leftColor = 'white'
-    //     } else {
-    //         this.leftActive = false
-    //         this.leftColor = 'lightgray'
-    //     }
-    // }
 
     this.multiplyAddend = () => {
-        // if (this.leftActive) {
-            this.addend *= this.multiplier
-            this.multiplier = this.multiplier + 1
-            this.subtractCost(this.leftCost)
-            this.leftCost = this.leftCost + 5
-            // updatePage()
-            save()
-        // }
+        this.addend *= this.multiplier
+        this.multiplier = this.multiplier + 1
+        this.subtractCost(this.leftCost)
+        this.leftCost = this.leftCost + 5
+        save()
     }
 
     this.add = () => {
         this.total += this.addend
         console.log(this.total)
-        // updatePage()
         save()
     }
 
     this.subtractCost = (cost) => {
         this.total = this.total - cost
-        // updatePage()
         save()
     }
 
@@ -64,32 +35,28 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
         intervals.push($interval(this.add, 1000));
         this.subtractCost(this.rightCost)
         this.rightCost = this.rightCost + 10
-        // updatePage()
         save()
     }
 
     this.reset = () => {
-        // if (this.resetActive) {
-            for (let x = 0; x < intervals.length; x++) {
-                clearInterval(intervals[x]);
-                $interval.cancel(intervals[x])
-            }
-            this.multiplier = INITIAL_MULTIPLIER
-            this.addend = 1
-            this.total = 0
-            this.leftCost = INITIAL_LEFT_COST
-            this.rightCost = INITIAL_RIGHT_COST
-            this.autoclickerCount = 0
-            this.rightActive = false
-            this.rightColor = 'lightgray'
-            this.leftActive = false
-            this.leftColor = 'lightgray'
-            this.resetActive = false
-            this.resetColor = 'lightgray'
-            intervals = []
-            // updatePage()
-            save()
-        // }
+        for (let x = 0; x < intervals.length; x++) {
+            clearInterval(intervals[x]);
+            $interval.cancel(intervals[x])
+        }
+        this.multiplier = INITIAL_MULTIPLIER
+        this.addend = 1
+        this.total = 0
+        this.leftCost = INITIAL_LEFT_COST
+        this.rightCost = INITIAL_RIGHT_COST
+        this.autoclickerCount = 0
+        this.rightActive = false
+        this.rightColor = 'lightgray'
+        this.leftActive = false
+        this.leftColor = 'lightgray'
+        this.resetActive = false
+        this.resetColor = 'lightgray'
+        intervals = []
+        save()
     }
 
     save = () => {
@@ -139,7 +106,6 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
         }
 
         intervals = [];
-        // updatePage();
         if (this.autoclickerCount > 0) {
             for (let y = 0; y < this.autoclickerCount; y++) {
                 intervals.push($interval(this.add, 1000));
