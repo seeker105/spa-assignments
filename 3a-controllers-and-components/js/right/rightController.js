9eab1f163d8201fbcde2e046f0dcd104f635b4f6
seeker105 <karavol27@gmail.com>
2017-09-28 11:34:46 -0500

    Remove unused autoclicker methods from rightController

--- 3a-controllers-and-components/js/right/rightController.js
+++ 3a-controllers-and-components/js/right/rightController.js
@@ -4,23 +4,4 @@ angular.module('clickerApp').controller('rightController', ['$interval', 'additi
 
     this.canClick = () => this.additionService.total < this.additionService.rightCost
 
-    intervals = []
-    this.startAutoclicker = () => {
-        if (additionService.rightActive){
-            // additionService.rightCost = additionService.rightCost + 10
-            additionService.autoclickerCount = additionService.autoclickerCount + 1;
-            intervals.push($interval(additionService.add, 1000))
-            additionService.subtractCost(additionService.rightCost)
-        }
-    }
-
-    this.stopAutoclickers = () => {
-        for (let x = 0; x < intervals.length; x++){
-            $interval.cancel(intervals[x])
-        }
-    }
-    
-
-
-
 }])
