e14e03813c1eaf547f5064df92bc87eb5de33b30
seeker105 <karavol27@gmail.com>
2017-09-28 12:09:49 -0500

    Implemented angular control on Reset button. Preparing to delete now-unused code

--- 3a-controllers-and-components/js/additionService.js
+++ 3a-controllers-and-components/js/additionService.js
@@ -17,23 +17,23 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
     this.resetActive = false
     this.resetColor = 'lightgray'
 
-    updatePage = () => {
-        if (this.total > 0 || this.addend > 1 || this.autoclickerCount > 0) {
-            this.resetActive = true
-            this.resetColor = 'white'
-        } else {
-            this.resetActive = false
-            this.resetColor = 'lightgray'
-        }
+    // updatePage = () => {
+    //     if (this.total > 0 || this.addend > 1 || this.autoclickerCount > 0) {
+    //         this.resetActive = true
+    //         this.resetColor = 'white'
+    //     } else {
+    //         this.resetActive = false
+    //         this.resetColor = 'lightgray'
+    //     }
 
-        if (this.total >= this.leftCost) {
-            this.leftActive = true
-            this.leftColor = 'white'
-        } else {
-            this.leftActive = false
-            this.leftColor = 'lightgray'
-        }
-    }
+    //     if (this.total >= this.leftCost) {
+    //         this.leftActive = true
+    //         this.leftColor = 'white'
+    //     } else {
+    //         this.leftActive = false
+    //         this.leftColor = 'lightgray'
+    //     }
+    // }
 
     this.multiplyAddend = () => {
         // if (this.leftActive) {
@@ -41,7 +41,7 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
             this.multiplier = this.multiplier + 1
             this.subtractCost(this.leftCost)
             this.leftCost = this.leftCost + 5
-            updatePage()
+            // updatePage()
             save()
         // }
     }
@@ -49,13 +49,13 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
     this.add = () => {
         this.total += this.addend
         console.log(this.total)
-        updatePage()
+        // updatePage()
         save()
     }
 
     this.subtractCost = (cost) => {
         this.total = this.total - cost
-        updatePage()
+        // updatePage()
         save()
     }
 
@@ -64,12 +64,12 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
         intervals.push($interval(this.add, 1000));
         this.subtractCost(this.rightCost)
         this.rightCost = this.rightCost + 10
-        updatePage()
+        // updatePage()
         save()
     }
 
     this.reset = () => {
-        if (this.resetActive) {
+        // if (this.resetActive) {
             for (let x = 0; x < intervals.length; x++) {
                 clearInterval(intervals[x]);
                 $interval.cancel(intervals[x])
@@ -87,9 +87,9 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
             this.resetActive = false
             this.resetColor = 'lightgray'
             intervals = []
-            updatePage()
+            // updatePage()
             save()
-        }
+        // }
     }
 
     save = () => {
@@ -139,7 +139,7 @@ angular.module('clickerApp').service('additionService', ['$interval', function (
         }
 
         intervals = [];
-        updatePage();
+        // updatePage();
         if (this.autoclickerCount > 0) {
             for (let y = 0; y < this.autoclickerCount; y++) {
                 intervals.push($interval(this.add, 1000));
--- 3a-controllers-and-components/js/header/headerController.js
+++ 3a-controllers-and-components/js/header/headerController.js
@@ -2,4 +2,6 @@ angular.module('clickerApp').controller('headerController', ['additionService', 
 
     this.additionService = additionService
 
+    this.canClick = () => !(this.additionService.total > 0 || this.additionService.addend > 1 || this.additionService.autoclickerCount > 0
+)
 }])
